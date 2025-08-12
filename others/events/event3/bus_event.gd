7e4e4ee050cf1e1750dad9c06a11601ce982d14c
kawazzaki98 <nachreda02@gmail.com>
2025-08-11 21:58:07 -0700

    add new place

---
+++ others/events/event3/bus_event.gd
@@ -0,0 +1,17 @@
+extends event;
+
+
+
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if(body.is_in_group("player")):
+		if(EventManager.start_event(event_index)):
+			interact();
+		pass
+	pass # Replace with function body.
+
+func interact():
+	$AnimationPlayer.play("event")
+	EventManager.complete_event(event_index);
+	pass
